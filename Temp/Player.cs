43f33776b79cf4fa9cbafd1ffba199c53f26b49e
SandervanGeel <sandervangeel24@gmail.com>
2018-11-05 21:56:05 +0100

    basic classes added

---
+++ Temp/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    class Player : TeamMember
+    {
+        #region variables
+        // Player info
+        private uint _number;
+        private bool _isCaptain;
+        
+        // total goals
+        private uint _total_goals;
+        private uint _total_misses;
+        private uint _total_attempts;
+
+        // special goals
+        private uint _total_7m_goals;
+        private uint _total_7m_misses;
+        private uint _total_7m_attempts;
+
+        private uint _total_break_goals;
+        private uint _total_break_misses;
+        private uint _total_break_attempts;
+
+        #endregion
+
+        #region getters
+        public uint getNumber() { return this._number; }
+        public bool getIsCaptain() { return this._isCaptain; }
+
+        public uint getTotalGoals() { return this._total_goals; }
+        public uint getTotalAttempts() { return this._total_attempts; }
+        public uint getTotal7mAttempts() { return this._total_7m_attempts; }
+        public uint getTotal7mGoals() { return this._total_7m_goals; }
+        public uint getTotalBreakAttempts() { return this._total_break_attempts; }
+        public uint getTotalBreakGoals() { return this._total_break_goals; }
+
+
+        // percentages
+        public uint getScorePercentage() { return (this._total_goals * 100) / this._total_attempts; }
+        public uint get7mPercentage() { return (this._total_7m_goals * 100) / this._total_7m_attempts; }
+        public uint getbreakPercentage() { return (this._total_break_goals * 100) / this._total_break_attempts; }
+        #endregion
+
+
+
+
+    }
+}
---
+++ Temp/TeamMember.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    class TeamMember
+    {
+        #region variables
+        // member info
+        private string _name;
+        private uint _weight;
+        private uint _length;
+        private uint _age;
+
+        private bool _isActive;
+
+        // cards
+        private bool _hasYellowCard;
+        private bool _hasRedCard;
+        private bool _hasBlueCard;
+
+        private uint _amount_suspension;
+        private bool _suspension_Active;
+        #endregion
+
+        #region simple getters
+        public string getName() { return this._name; }
+        public uint getWeight() { return this._weight; }
+        public uint getLength() { return this._length;  }
+        public uint getAge() { return this._age; }
+
+        public bool getIsActive() { return this._isActive;  }
+        public bool getHasYellowCard() { return this._hasYellowCard;  }
+        public bool getHasRedCard() { return this._hasRedCard; }
+        public bool getHasBlueCard() { return this._hasBlueCard; }
+        public uint getAmountOfSuspensions() { return this._amount_suspension; }
+        public uint getSuspensionActive() { return this._suspension_Active; }
+        #endregion
+
+        #region setters
+        // info
+        public void SetName(string name)
+        {
+            if (name.Length >= 2 && name.Length <= 20)
+            {
+                this._name = name;
+            }
+        }
+        public void setWeight(uint weight)
+        {
+            if (weight >= 40 && weight <= 200)
+            {
+                this._weight = weight;
+            }
+        }
+        public void setLength(uint length)
+        {
+            if (length >= 100 && length <= 230)
+            {
+                this._length = length;
+            }
+        }
+        public void setAge(uint age)
+        {
+            if(age >= 14 && age <= 50 )
+            {
+                this._age = age;
+            }
+        }
+
+        //cards
+        public void setYellowCard() { this._hasYellowCard = true; }
+        public void resetYellowCard() { this._hasYellowCard = false; }
+        public void setRedCard() { this._hasRedCard = true; }
+        public void resetRedCard() { this._hasRedCard = false; }
+        public void setBlueCard() { this._hasBlueCard = true; }
+        public void resetBlueCard() { this._hasBlueCard = false; }
+        
+        //suspension
+        public void addSuspension()
+        {
+            if(this._amount_suspension <= 2)
+            {
+                this._amount_suspension++;
+            }
+        }
+        public void substrSuspension()
+        {
+            if(this._amount_suspension > 0)
+            {
+                this._amount_suspension--;
+            }
+        }
+        public void resetSuspension()
+        {
+            this._amount_suspension = 0;
+        }
+
+        public void setSuspensionActive() { this._suspension_Active = true; }
+        public void resetSuspensionActive() { this._suspension_Active = false; }
+
+        #endregion
+    }
+}
